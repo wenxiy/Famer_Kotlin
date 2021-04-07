7fe321373ee35eb96583b7133afabd7e252a059e
wenxiy <193367189@qq.com>
2021-04-08 00:22:27 +0800

    first

--- app/src/main/java/com/example/test/data/AccountBean.kt
+++
@@ -1,8 +0,0 @@
-package com.example.test.data
-
-data class AccountBean(
-    var name: String = "",
-    var id: String = "",
-    var password: String = ""
-
-)
---
+++ smart-farm/src/main/java/com/example/test/data/AccountBean.kt
@@ -0,0 +1,7 @@
+package com.example.test.data
+
+data class AccountBean(
+    var name: String = "",
+    var id: String = "",
+    var password: String = ""
+)
