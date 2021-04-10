0c1d20b5caf1e01e67f292820ca61c906f3f36ed
wenxiy <193367189@qq.com>
2021-04-10 21:44:55 +0800

    note

--- smart-farm/src/main/java/com/example/test/Login/iokit/ui/DataViewModel.kt
+++ smart-farm/src/main/java/com/example/test/Login/iokit/ui/DataViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.ViewModel
 import com.example.test.data.AccountBean
 import com.example.test.Login.iokit.export.LoginModel
 
+/**
+ *
+ */
 class DataViewModel : ViewModel() {
     private var loginModel = LoginModel()
     private var accountBean = MutableLiveData<List<AccountBean>>()
--- smart-farm/src/main/java/com/example/test/Login/iokit/ui/LoginActivity.kt
+++ smart-farm/src/main/java/com/example/test/Login/iokit/ui/LoginActivity.kt
@@ -12,6 +12,12 @@ import com.example.test.R
 import com.example.test.databinding.ActivityLoginBinding
 import kotlinx.android.synthetic.main.activity_login.*
 
+/**
+ * 使用ViewModel+DataBinding+Livedata来进行
+ * ①利用DataBindingUtil来进行对View(XML)的绑定
+ * ②利用binding.lifecycleOwner = this 来进行LiveData的绑定
+ * ③利用binding.ViewModel = viewModel 来进行UI控制器Binding与ViewModel进行单向通信
+ */
 class LoginActivity : AppCompatActivity() {
     private val viewModel by lazy {
         ViewModelProviders.of(this).get(DataViewModel::class.java)
@@ -24,7 +30,7 @@ class LoginActivity : AppCompatActivity() {
             R.layout.activity_login
         )
         binding.lifecycleOwner = this  // use Fragment.viewLifecycleOwner for fragments
-        // binding.viewmodel = viewModel
+         binding.viewModel = viewModel
         register_go.setOnClickListener(View.OnClickListener {
             Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
             val intent = Intent(this, MainPageActivity::class.java)
